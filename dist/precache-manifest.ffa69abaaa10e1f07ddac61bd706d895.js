283c7da9a39d193023cd3c35484b1cc312a03b7e
zhoukai <1301524439@qq.com>
2019-01-23 18:18:37 +0800

    tt

---
+++ dist/precache-manifest.ffa69abaaa10e1f07ddac61bd706d895.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "95b1f44843da81949b23",
+    "url": "/js/chunk-vendors.2209ae2f.js"
+  },
+  {
+    "revision": "b987dc4fc1b8235b5405",
+    "url": "/js/app.2d8ebcfc.js"
+  },
+  {
+    "revision": "2ff972681ae86985a6f7433a1837feec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "95b1f44843da81949b23",
+    "url": "/css/chunk-vendors.9c8e3772.css"
+  },
+  {
+    "revision": "b987dc4fc1b8235b5405",
+    "url": "/css/app.feede462.css"
+  }
+];
